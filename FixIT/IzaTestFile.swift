21b671313d472831fef2920ce26c3c7f24617979
Izabek <izabekz@gmail.com>
2018-07-13 17:17:24 +0600

    iza test file added

---
+++ FixIT/IzaTestFile.swift
@@ -0,0 +1,11 @@
+//
+//  IzaTestFile.swift
+//  FixIT
+//
+//  Created by Izabek on 7/13/18.
+//  Copyright © 2018 Mikowa. All rights reserved.
+//
+
+import Foundation
+
+//this file was created as test
