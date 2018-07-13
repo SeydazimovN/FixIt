9f95051c95d03b2533cc650b6fe41f7aa2433a0a
Izabek <izabekz@gmail.com>
2018-07-13 17:52:31 +0600

    new files added

---
+++ FixIT/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  FixIT
+//
+//  Created by Izabek on 7/13/18.
+//  Copyright © 2018 Mikowa. All rights reserved.
+//
+
+import Foundation
---
+++ FixIT/FileTestIza.swift
@@ -0,0 +1,9 @@
+//
+//  FileTestIza.swift
+//  FixIT
+//
+//  Created by Izabek on 7/13/18.
+//  Copyright © 2018 Mikowa. All rights reserved.
+//
+
+import Foundation
